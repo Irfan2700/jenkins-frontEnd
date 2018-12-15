076c66dd2c728636ace8da0a1743d6947a2ff5bb
Irfan Rizvi <sirizvi2700@gmail.com>
2018-12-15 12:22:08 +0530

    Intial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: '/login', component: LoginComponent},
+  {path: '/signup', component: SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
